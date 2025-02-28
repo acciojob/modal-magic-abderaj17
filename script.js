cb93a93947e0020a433e11cf515cfa9c7fed101a
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-28 16:09:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const btn = document.getElementById('btn');
+const closeBtn = document.getElementById('close-btn')
+const display = document.querySelector('.modal-container');
+
+btn.addEventListener('click', ()=>{
+	display.style.display = "block"; 
+})
+closeBtn.addEventListener('click', ()=>{
+	display.style.display = "none";
+})
+window.addEventListener('click', (e)=>{
+	if(e.target == display){
+		display.style.display = 'none';
+	}
+})
